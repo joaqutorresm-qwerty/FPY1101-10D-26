256fa7163fac542f0c6b469140c32d32e06b7092
joaqutorresm-qwerty <joaqu.torresm@duocuc.cl>
2026-05-22 16:26:53 -0400

    AprendiendoFunc V - 1.0

---
+++ EjerciciosFunciones/AprendiendoFunc V - 1.0.py
@@ -0,0 +1,26 @@
+#Sin datos en los parentesis
+def sumarFijo():
+    num1 = 2
+    num2 = 3
+    sumar = num1 + num2
+    print(sumar)
+
+#Si se colocan valores en los parentesis se llaman PARAMETROS
+def sumar(num1, num2)-> int:
+    """Esta función sirve para sumar números cualquiera"""
+    sumar = num1 + num2
+    print(sumar)
+
+#Restar con def y RETURN
+def restar(num1 , num2):
+    restar = num1 - num2
+    return restar
+
+sumarFijo()
+sumar(3,4)
+n1 = int(input("Ingrese un número: "))
+n2 = int(input("Ingrese otro número: "))
+sumar(n1 , n2)
+print(restar(n1 , n2))
+
+#Se puede importar funciones con: import funciones as fn (creadas en otro archivo)
