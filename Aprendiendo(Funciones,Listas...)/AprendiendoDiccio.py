4eb324759988224309b7a564248605b5d72342f9
joaqutorresm-qwerty <you@example.com>
2026-05-27 14:46:17 -0400

    AprendiendoDiccio V - 1.0

---
+++ Aprendiendo(Funciones,Listas...)/AprendiendoDiccio.py
@@ -0,0 +1,33 @@
+#uso de diccionarios
+diccionario = {
+    "Rut" : "22617787-6",
+    "Nombre" : "Joaquín",
+    "Telefono" : [1234,567,890],
+    "Dirección" : "Quilpué"
+}
+
+#te permite imprimir un valor del diccionario, entre los corchetes hay que poner el dato elegido
+print(diccionario["Nombre"])
+#el n*1 es para elegir el dato seleccionado, si se escoje el 2 deberia mostrar: 890
+print(diccionario["Telefono"][1])
+
+#podemos insertar datos o claves de la siguiente manera:
+diccionario["email"] = "joaqut@gmail.com"
+print(diccionario["email"])
+
+#ingresando un número más al diccionario
+diccionario["Telefono"].append(190)
+print(diccionario["Telefono"])
+
+#actualizando...
+diccionario["Telefono"][0] = 666
+print(diccionario["Telefono"])
+diccionario["Dirección"] = "Viña del Mar"
+print(diccionario)
+
+#borrando...
+del diccionario["Dirección"]
+print(diccionario)
+print(diccionario["Telefono"])
+diccionario["Telefono"].pop(2)
+print(diccionario["Telefono"])
