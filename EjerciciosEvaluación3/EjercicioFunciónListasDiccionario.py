d6b218e3bd64d535f15e59785f34d85227d96412
joaqutorresm-qwerty <joaqu.torresm@duocuc.cl>
2026-06-01 17:28:42 -0400

    ShopMenu V - 1.1

--- EjerciciosEvaluación3/EjercicioFunciónListasDiccionario.py
+++ EjerciciosEvaluación3/EjercicioFunciónListasDiccionario.py
@@ -17,11 +17,24 @@ def agregar_producto(productos):
         return
 
     stock = int(input("Ingrese stock: "))
-    precio = int(input("Ingrese el precio$: "))
+    precio = int(input("Ingrese el precio: $"))
 
     productos[nombre] = [stock,precio]
     print("Productos agregados correctamente!")
 
+def mostrar_productos(productos):
+    mostrar = (productos)
+    print(f"A continuación se le mostrara todos los productos, junto a su precio y existencia... {mostrar}")
+
+def buscar_productos(productos):
+    buscar = (productos)
+    buscar = str(input("Ingrese el producto que quiere buscar: "))
+    if buscar in (["Mouse"]):
+        print(["Mouse"][0][1])
+
+def producto_mas_caro(productos):
+    mascaro = max(productos.values())
+    print(mascaro)
 opcion_menu = 0
 
 while True:
@@ -40,14 +53,14 @@ while True:
     
     if opcion_menu == 1:
         agregar_producto(productos)
-    #elif opcion_menu == 2:
-        #mostrar_productos(productos)
-    #elif opcion_menu == 3:
-        #buscar_productos(productos)
-    #elif opcion_menu == 4:
-        #producto_mas_caro(productos)
-    #elif opcion_menu == 5:
-        #print("Fin del programa...")
-        #break
+    elif opcion_menu == 2:
+        mostrar_productos(productos)
+    elif opcion_menu == 3:
+        buscar_productos(productos)
+    elif opcion_menu == 4:
+        producto_mas_caro(productos)
+    elif opcion_menu == 5:
+        print("Fin del programa...")
+        break
     else:
         print("Opción inválida!")
