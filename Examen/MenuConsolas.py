6302e4a2879b23bb22c34a8571ef7106542ac4e6
Joaquín <personalzynops@gmail.com>
2026-07-05 23:14:06 -0400

    MenuConsolas - V1.0

---
+++ Examen/MenuConsolas.py
@@ -0,0 +1,158 @@
+consolas = {}
+ventas = {}
+
+def menu():
+    print("#"*30)
+    print("1. Agregar consola")
+    print("2. Buscar consola por sigla")
+    print("3. Eliminar consola")
+    print("4. Mostrar todas las consolas")
+    print("5. Salir")
+    print("#"*30)
+
+def opcion():
+    while True:
+        try:
+            op = int(input("Seleccione su opción: "))
+            return op
+        except ValueError:
+            print("Seleccione una opción valida")
+
+def agregar_consola(consolas, ventas):
+    sigla = input("Ingrese la sigla: ").strip()
+    if not validacion_sigla(sigla):
+        print("Ingrese una sigla valida")
+        return
+    if validacion_sigla_existente(consolas, sigla):
+        print("Esta sigla ya existe")
+        return
+    
+    nombre = input("Ingrese el nombre: ")
+    if not validacion_nombre(nombre):
+        print("Ingrese un nombre valido")
+        return
+    
+    fabricante = input("Ingrese el fabricante: ")
+    if not validacion_fabricante(fabricante):
+        print("Ingrese un fabricante valido")
+        return
+    
+    ano_lanzamiento = convertir_entero(input("Ingrese el ano de lanzamiento"))
+    if ano_lanzamiento is False or not validacion_anolanzamiento(ano_lanzamiento):
+        print("Ingrese un ano valido")
+        return
+    
+    precio = convertir_float(input("Ingrese el precio: "))
+    if precio is False or not validacion_precio(precio):
+        print("Ingrese un precio valido")
+        return
+    
+    stock = convertir_entero(input("Ingrese el stock disponible: "))
+    if stock is False or not validacion_stock(stock):
+        print("Ingrese un stock valido..")
+        return
+    
+    consolas[sigla] = [nombre, fabricante, ano_lanzamiento] # se puede hacer de tal manera: consolas[sigla] = [nombre, fabricante, int(ano_lanzamiento)]
+    ventas[sigla] = [precio, stock] 
+    print("Se ha agregado la consola.")
+    print(type(consolas[sigla][0])) 
+    print(type(consolas[sigla][1]))
+    print(type(consolas[sigla][2]))
+    print(type(ventas[sigla][0])) 
+    print(type(ventas[sigla][1]))
+
+def validacion_sigla(sigla):
+    if not (2 <= len(sigla) <= 5):
+        return False
+    if not sigla.isalnum():
+        return False
+    if not sigla.isupper():
+        return False
+    else:
+        return True
+
+def validacion_sigla_existente(consolas, sigla):
+    if sigla in consolas:
+        return True
+    
+
+def validacion_nombre(nombre):
+    if not (3 <= len(nombre) <= 40):
+        return False
+    else:
+        return True
+
+def validacion_fabricante(fabricante):
+    if not (2 <= len(fabricante) <= 30):
+        return False
+    else:
+        return True
+
+def validacion_anolanzamiento(ano_lanzamiento): # .isdigit
+    if not (1972 <= ano_lanzamiento <= 2025):
+        return False
+    else:
+        return True
+
+def validacion_precio(precio): 
+    try:
+        if not precio > 0:
+            return False
+        else:
+            return True
+    except ValueError:
+        return False
+
+def validacion_stock(stock): # if not stock.isdigit return false, same as ano
+    if not stock >= 0:
+        return False
+    else:
+        return True
+    
+def convertir_entero(input):
+    try:
+        return int(input)
+    except ValueError:
+        return False 
+
+def convertir_float(input):
+    try:
+        return float(input)
+    except ValueError:
+        return False
+
+def buscar_consola(consolas, ventas):
+    sigla = input("Ingrese la consola a buscar (SIGLA): ")
+
+    if not sigla in consolas:
+        print("No hay consolas")
+        return False
+    else:
+        print("###### Consola Encontrada #####")
+        print(f"Sigla: {sigla}")
+        print(f"Nombre: {consolas[sigla][0]}")
+        print(f"Fabricante: {consolas[sigla][1]}")
+        print(f"Ano Lanz.: {consolas[sigla][2]}")
+        print(f"Precio: {ventas[sigla][0]}")
+        print(f"Stock: {ventas[sigla][1]}")
+        return True
+
+def eliminar_consola(consolas, ventas):
+    print("WIP")
+
+while True:
+    menu()
+    op = opcion()
+    if op == 1:
+        agregar_consola(consolas, ventas)
+    elif op == 2:
+        buscar_consola(consolas, ventas)
+    elif op == 3:
+        eliminar_consola(consolas, ventas)
+    elif op == 4:
+        print
+    elif op == 5:
+        print("Saliendo...")
+        break
+    else:
+        print("Escoja una opcion valida")
